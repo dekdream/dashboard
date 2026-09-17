8f7dcd415e784480dde4d82f2c5aed946a265a27
dekdream <dgcdreamer007@gmail.com>
2026-09-17 16:24:08 +0700

    Deploy latest dashboard build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "880419362"
+    serviceWorkerVersion: "3221649027"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "462ac38ef5b7cfdade99c377228eb61c",
+"flutter_bootstrap.js": "a8e8d01bf63fee85b720e2881236efb4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "62d1bac18afeaa8bd5aaa46a26507f33",
 "/": "62d1bac18afeaa8bd5aaa46a26507f33",
-"main.dart.js": "a855b1f9471f98cd7e3887fa13ee817d",
+"main.dart.js": "165639185697da9481d9ba0d46ea4695",
 "manifest.json": "de13d77f814d36247de7fbf6241abf01",
 "version.json": "72bed768eec70c04df83e2ce821daee2"};
 // The application shell files that are downloaded before a service worker can
